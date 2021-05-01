20f6899df17a3944aca77f8dee63bd4b2c6bc530
yurec0098 <yurec0098@rambler.ru>
2021-05-01 19:51:49 +0300

    Написать программу, выводящую введенную пользователем строку в обратном порядке (olleH вместо Hello).

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -6,7 +6,18 @@ namespace Lesson_3
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Вывод пользовательского ввода в инвертированном виде");
+			do
+			{
+				Console.WriteLine("Ожидание пользовательского ввода:");
+
+				string line = Console.ReadLine();
+				for (int i = line.Length - 1; i >= 0; i--)
+					Console.Write(line[i]);
+				Console.WriteLine();
+
+				Console.WriteLine("Попробуем ещё? Тогда введите +");
+			} while (Console.ReadLine() == "+");
 		}
 	}
 }
