6f4cb2a94bce91b98b3b871a67ed841ec1a60996
yurec0098 <yurec0098@rambler.ru>
2021-05-05 20:16:18 +0300

    Написать программу, выводящую элементы двухмерного массива по диагонали.

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -6,7 +6,32 @@ namespace Lesson_3
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[,] array = new string[10, 10];
+			for (int i = 0; i < 10; i++)
+				for (int j = 0; j < 10; j++)
+					array[i, j] = $"{(char)(65 + i)}{j}";
+
+			string[] diagonal = new string[10];
+			string[] diagonal2 = new string[10];
+			for (int i = 0; i < 10; i++)
+				for (int j = 0; j < 10; j++)
+				{
+					if (i == j)
+						diagonal[i] = array[i, j];
+					if (9 -i == j)
+						diagonal2[i] = array[i, j];
+				}
+
+			Console.WriteLine(string.Join(", ", diagonal));
+			Console.WriteLine(string.Join(", ", diagonal2));
+			Console.WriteLine();
+
+			for (int i = 0; i < 10; i++)
+			{
+				for (int j = 0; j < 10; j++)
+					Console.Write($"{array[i, j]} ");
+				Console.WriteLine();
+			}
 		}
 	}
 }
